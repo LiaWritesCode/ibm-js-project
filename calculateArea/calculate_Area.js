b7cbf81948a2b634cc0a95fefbbe8b141b940448
LiaWritesCode <liaramos.dev@outlook.com>
2025-05-04 20:00:04 -0400

    Added practice tasks to files for presentation purposes.

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -12,3 +12,15 @@ let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 
 }
+
+//Practice Task: JavaScript
+
+function groceryTracker () {
+    let grocery1 = parseFloat(document.getElementById('grocery1').value) || 0;
+    let grocery2 = parseFloat(document.getElementById('grocery2').value) || 0;
+    let grocery3 = parseFloat(document.getElementById('grocery3').value) || 0;
+
+let grandtotal = grocery1 + grocery2 + grocery3;
+
+document.getElementById('total').innerText = `The total amount is: ${grandtotal.toFixed(2)}`;
+}
