7b0546c849c0e9d05674f01a16b109afc9d2a13c
LiaWritesCode <liaramos.dev@outlook.com>
2025-05-05 15:05:37 -0400

    Restaurant Menu Folder, JS, HTML files with initial code.

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,20 @@
+// Menu Arrays
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const maincourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// Map Method
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// forEach - Main Course Menu
+let maincourseTotalItems = '' ;
+    maincourseMenu.forEach((item, index) => {
+    maincourseTotalItems += `<p>Item ${index + 1}: ${item}</p>`;});
+    document.getElementById('maincourseMenuItems').innerHTML = maincourseTotalItems;
+
+// Dessert Menu forLoop
+let dessertItem = '' ;
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+    document.getElementById('dessertMenuItems').innerHTML = dessertItem;
